e28503717de0738d6358aaa2e2d14f20e5c97886
boermacon <boermacon@gmail.com>
2023-04-10 15:04:39 -0400

    Server Code

---
+++ my-server.ts
@@ -0,0 +1,24 @@
+import { randomInt } from "crypto";
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+const PORT = process.env.PORT ?? 8000; // Allow dynamic PORT setting (Heroku)
+
+const words = ["CIGAR", "REBUT", "SISSY", "HUMPH", "AWAKE", "BLUSH", "FOCAL", "EVADE", "NAVAL", "SERVE", "HEATH", "DWARF", "MODEL", "KARMA", "STINK", "GRADE", "QUIET", "BENCH", "ABATE", "FEIGN", "MAJOR", "DEATH", "FRESH", "CRUST", "STOOL", "COLON"]
+
+
+
+// Define GET endpoint(s)
+/*
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+});*/
+
+app.get("/getWord", (req: Request, res: Response) => {
+    res.header({"Access-Control-Allow-Origin": "*"});
+    res.send({ secret: words[Math.floor(Math.random() * words.length)]}); // Opt-4: send JSON response
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening to port ${PORT}`);
+});
