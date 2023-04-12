6d46e3a0cf0d1b5b616c58ac9eaf70e46833164c
boermacon <boermacon@gmail.com>
2023-04-12 10:17:16 -0400

    update to tsconfig

--- dist/my-server.js
+++ dist/my-server.js
@@ -1,8 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 var _a;
-import express from "express";
-import cors from "cors";
-const app = express();
-app.use(cors());
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 8000; // Allow dynamic PORT setting (Heroku)
 const words = ["CIGAR", "REBUT", "SISSY", "HUMPH", "AWAKE", "BLUSH", "FOCAL", "EVADE", "NAVAL", "SERVE", "HEATH", "DWARF", "MODEL", "KARMA", "STINK", "GRADE", "QUIET", "BENCH", "ABATE", "FEIGN", "MAJOR", "DEATH", "FRESH", "CRUST", "STOOL", "COLON"];
 // Define GET endpoint(s)
--- my-server.ts
+++ my-server.ts
@@ -1,5 +1,5 @@
 import { randomInt } from "crypto";
-import express, { Application, Request, Response } from "express";
+import express, { Express, Application, Request, Response } from "express";
 import cors from "cors";
 const app: Application = express();
 app.use(cors());
